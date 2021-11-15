ba02c97cf2cde42cf75805464bc04aad9fbbce45
Ashwin Aashu <80351416+AshwinAashu@users.noreply.github.com>
2021-11-15 19:32:00 +0530

    bottom-pad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Operators from "./components/Operators";
 import NumberButtons from "./components/NumberButtons";
+import BottomPad from "./components/BottomPad";
 export default function App() {
   return (
     <div className="App">
       <Operators />
       <NumberButtons />
+      <BottomPad />
     </div>
   );
 }
---
+++ src/components/BottomPad.jsx
@@ -0,0 +1,10 @@
+const BottomPad = () => {
+  return (
+    <div className="bttoom-pad">
+      <button className="button-bottom">.</button>
+      <button className="button-bottom">0</button>
+      <button className="button-bottom">=</button>
+    </div>
+  );
+};
+export default BottomPad;
